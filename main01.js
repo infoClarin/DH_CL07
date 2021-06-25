6138ea088fb4e92da87a4a88990a70686629ab3b
Bart Allen <wulff2099@gmail.com>
2021-06-25 15:53:31 -0300

    Archivos Subidos por Sebastian, Index01.html, main01.css y main01.js

---
+++ main01.js
@@ -0,0 +1,12 @@
+var auto2 = {
+    marca: "Fordd",
+    modelo: "Fiesta",
+    precio: 150000,
+    km: 200,
+    color: "Azul",
+    cuotas: 12,
+    anio: 2019,
+    patente: "APL123",
+    vendido: false
+}
+module.exports = auto2;
