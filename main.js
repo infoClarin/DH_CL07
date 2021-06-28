e9c726bde3a9cef29e8fbbd7cacf591783195965
Hugo Vasiliev <infografiaclarin@gmail.com>
2021-06-28 16:09:20 -0300

    Arreglé el js y sumé un comentario, lo activé para velo en la web

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var string = "Veamos cómo se usa Git, parece bastante fácil...";
+// Qué hacemos con tu nueva variable?:) 
+//El código anda de vuelta, si lo mirás en la web en https://infoclarin.github.io/DH_CL07/
 var nuevoString = " Modificanción del Archivo main.js";
 var str = string.split("");
 var este = document.getElementById('str');
 (function animate() {
-    str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
+    str.length > 0 ? este.innerHTML += str.shift() : clearTimeout(running);
     var running = setTimeout(animate, 90);
 })();
